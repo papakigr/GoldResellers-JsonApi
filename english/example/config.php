b0f0be16b35bfbf032a97e2d2a4b87a3815101fc
voulacsd <voulacsd@hotmail.com>
2014-09-05 12:47:26 +0300

    Update config.php

--- english/example/config.php
+++ english/example/config.php
@@ -3,7 +3,7 @@
 	
 
 	
-	
+	//settings
 	$Papaki_admin_email = "test@gmail.com";		//Place  your Email here
 	 $papaki_Post_url="https://api.papaki.gr/register_url2.aspx";
 	$Papaki_apikey="myapikey";
